7fdb166cbdf806c5702f7145ed6fa6359ed813a3
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-09-28 12:53:30 +0700

    Stage handler v0.1

---
+++ Assets/Scripts/stageHandler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class stageHandler : MonoBehaviour {
+
+	//ambil UI Text
+	public Text textUI;
+
+	//Init variabel
+	//Jenis stage
+	//0 : Normal
+	//1 : Boss
+	public int jenisStage = 1;
+
+	//Timer
+	//dalam detik
+	public float timer;
+
+	//Jeda spawner
+	public float jedaSpawn;
+
+	//HP boss
+	public int bossHP;
+
+	// Use this for initialization
+	void Start () {
+		if(jenisStage == 0){
+			//Normal stage
+			//tampilin timer
+			setTextUI();
+		}
+		else{
+			//Boss stage
+			//spawn boss
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//jika normal stage dan timer habis
+		if ((jenisStage == 0 && timer <= 0) || (jenisStage == 1 && bossHP <= 0)) {
+			//akhiri game
+			textUI.text = "Game Over.";
+		}
+		else if (jenisStage == 0) {
+			//kurangi timer
+			timer -= Time.deltaTime;
+			//tampilin di gui
+			setTextUI();
+		}
+		else {
+			//tampilin hp boss
+			textUI.text = "Boss' HP = " + bossHP.ToString();
+		}
+	}
+
+	void setTextUI(){
+		textUI.text = "Time left : " + timer.ToString();
+	}
+}
