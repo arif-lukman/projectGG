4e0e6e02ffad8a51fa9fa23b644de72cd5a49d9d
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-09-29 00:51:18 +0700

    Fungsi bergerak v0.1

--- Assets/Scripts/playerHandler.cs
+++ Assets/Scripts/playerHandler.cs
@@ -5,74 +5,134 @@ using UnityEngine.UI;
 
 public class playerHandler : MonoBehaviour {
 
-	//var rigidbody2d player
-	public Rigidbody2D rb2d;
+	//enum player state
+	//player states
+	//1. top
+	//2. bot
+	public enum playerState {TOP, BOT, DOWN};
 
 	//var boxcollider2d player
 	public BoxCollider2D bc2d;
 
+	//var rigidbody2d player
+	public Rigidbody2D rb2d;
+
 	//var UI debug
 	public Text textUI;
 
-	//var debug
-	private Vector2 move;
-
 	//var kecepatan gerak
-	private float mvSpd = 0.5f;
+	public float movSpd;
 
 	//var kecepatan loncat
-	private float jmpSpd = 30f;
+	public float jmpSpd;
 
 	//var boleh loncat apa engga
 	private bool canJump;
+
+	//set state
+	public playerState state;
+
+	//objek upper platform
+	private GameObject top;
+
+	//nyimpen vektor2 arah
+	private Vector2 dir;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
 		bc2d = GetComponent<BoxCollider2D> ();
+		top = GameObject.FindWithTag ("Upper Platform");
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		//var buat ambil input horizontal
-		float hor = 0.0f;
+		//input horizontal
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			//loncat
-			hor = mvSpd;
+			//kanan
+			Move("RIGHT");
 		}
 		else if (Input.GetKey (KeyCode.LeftArrow)) {
-			//turun
-			hor = -mvSpd;
+			//kiri
+			Move("LEFT");
 		}
-
-		//var buat ambil input up down
-		float ver = 0.0f;
-		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+		//input vertikal
+		if (Input.GetKeyDown (KeyCode.UpArrow) && state == playerState.TOP) {
 			//loncat
-			ver = jmpSpd;
+			Jump("UPTOP");
 		}
-		else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+		else if (Input.GetKeyDown (KeyCode.UpArrow) && state == playerState.BOT) {
+			//loncat
+			Jump("UPBOT");
+		}
+		else if (Input.GetKeyDown (KeyCode.DownArrow) && state == playerState.TOP) {
 			//turun
-			ver = -jmpSpd;
+			Jump("DOWN");
+			state = playerState.DOWN;
 		}
-
-		//set vector perubahan
-		move = new Vector2(hor, ver);
-
-		//tampilin debug
-		tampilDebug();
-
-		//cek keyboard
-		//panah kanan = gerak ke kanan
-		//panah kiri = gerak ke kiri
-		rb2d.AddForce (move);
-
+		if(!canJump && transform.position.y > top.transform.position.y && state != playerState.DOWN){
+			Physics2D.IgnoreCollision (bc2d, top.GetComponent<BoxCollider2D> (), false);
+		}
 		//panah atas = loncat
 		//panah bawah kalo di platform kedua = loncat ke bawah
 	}
 
-	//Nampilin debug
-	void tampilDebug(){
-		textUI.text = "move = " + move.ToString ();
+	void OnCollisionStay2D(Collision2D collision){
+		//cek collision antara objek ini dengan lantai
+		if (collision.gameObject.tag == "Upper Platform" || collision.gameObject.tag == "Lower Platform") {
+			canJump = true;
+		}
+		//Debug.Log ("Can Jump = " + canJump);
+		if (collision.gameObject.tag == "Upper Platform") {
+			state = playerState.TOP;
+		}
+
+		if (collision.gameObject.tag == "Lower Platform") {
+			state = playerState.BOT;
+		}
+		//Debug.Log (state);
+		//Debug.Log ("Player is on the " + collision.gameObject.tag);
+	}
+
+	void OnCollisionExit2D(Collision2D collision){
+		//cek collision antara objek ini dengan lantai
+		if (collision.gameObject.tag == "Upper Platform" || collision.gameObject.tag == "Lower Platform") {
+			canJump = false;
+		}
+		//Debug.Log ("Can Jump = " + canJump);
+	}
+
+	//fungsi gerak
+	void Move(string _dir){
+		//gerak kanan
+		if (_dir == "RIGHT") {
+			dir = new Vector2 (movSpd, 0f);
+		} 
+		//gerak kiri
+		else if (_dir == "LEFT") {
+			dir = new Vector2 (-movSpd, 0f);
+		}
+		rb2d.AddForce (dir);
+	}
+
+	//fungsi loncat
+	void Jump(string _dir){
+		//loncat keatas
+		if (_dir == "UPTOP" || _dir == "UPBOT") {
+			dir = new Vector2 (0f, jmpSpd);
+		} 
+		//loncat kebawah
+		else if (_dir == "DOWN") {
+			dir = new Vector2 (0f, 0f);
+		}
+		//Debug.Log (_dir);
+		if (canJump) {
+			if (_dir != "UPTOP")
+				Physics2D.IgnoreCollision (bc2d, top.GetComponent<BoxCollider2D> (), true);
+			//bc2d.enabled = false;
+			rb2d.AddForce (dir);
+		}
+		//Debug.Log ("my y = " +transform.position.y);
+		//Debug.Log ("his y = " +top.transform.position.y);
 	}
 }
---
+++ Assets/enemyAIMelee.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyAIMelee : MonoBehaviour {
+
+	//box collider
+	private BoxCollider2D bc2d;
+
+	//rigidbody
+	private Rigidbody2D rb2d;
+
+	//states
+	//1. idle top
+	//2. idle bot
+	//3. aggro top
+	//4. aggro bot
+	//5. attacking
+	//6. jump up
+	//7. jump down
+	//8. can jump
+	//9. cannot jump
+	public enum enemyState {IDLETOP, IDLEBOT, AGGROTOP, AGGROBOT, ATTACK, JUMPUP, JUMPDOWN};
+
+	//kecepatan loncat
+	public float jmpSpd;
+
+	//kecepatan gerak
+	public float movSpd;
+
+	//HP
+	public int hp;
+
+	//damage
+	public int dmg;
+
+	//jeda serangan
+	public int atkDly;
+
+	//jarak aggro
+	public float aggDst;
+
+	//arah
+	private Vector2 dir;
+
+	//set state
+	private enemyState state;
+
+	//set jumpstate
+	private bool canJump;
+
+	//set objek player
+	private GameObject player;
+
+	//set script player
+	private playerHandler playerScript;
+
+	//objek platform
+	private GameObject top;
+	private GameObject bot;
+
+	void Start(){
+		player = GameObject.FindWithTag ("Player");
+		playerScript = player.GetComponent<playerHandler> ();
+		top = GameObject.FindWithTag ("Upper Platform");
+		rb2d = GetComponent<Rigidbody2D> ();
+		bc2d = GetComponent<BoxCollider2D> ();
+	}
+
+	void FixedUpdate(){
+		//Debug.Log ("State = " +state);
+		//ambil jarak antara player dan musuh
+		float dist = Vector2.Distance (transform.position, player.transform.position);
+		//Debug.Log( "Jarak = " + dist );
+
+		//kalau masuk jarak, aggro
+		if ( dist <= aggDst ) {
+			if (state == enemyState.IDLETOP) {
+				//aggro upper platform
+				state = enemyState.AGGROTOP;
+			}
+			else if (state == enemyState.IDLEBOT) {
+				//aggro lower platform
+				state = enemyState.AGGROBOT;
+			}
+		}
+
+		//panggil fungsi state handling
+		StateHandling();
+	}
+
+	void OnCollisionStay2D(Collision2D collision){
+		//cek collision antara objek ini dengan lantai
+		if (collision.gameObject.tag == "Upper Platform" || collision.gameObject.tag == "Lower Platform") {
+			canJump = true;
+		}
+		//Debug.Log ("Can Jump = " + canJump);
+
+		if (collision.gameObject.tag == "Upper Platform") {
+			state = enemyState.IDLETOP;
+		}
+
+		if (collision.gameObject.tag == "Lower Platform") {
+			state = enemyState.IDLEBOT;
+		}
+	}
+
+	void OnCollisionExit2D( Collision2D collision ){
+		//cek collision antara objek ini dengan lantai
+		if (collision.gameObject.tag == "Upper Platform" || collision.gameObject.tag == "Lower Platform") {
+			canJump = false;
+		}
+	}
+
+	//fungsi state handling
+	void StateHandling(){
+		switch (state) {
+		case enemyState.AGGROTOP:
+			//panggil fungsi aggrotop
+			Aggro("TOP");
+			break;
+		case enemyState.AGGROBOT:
+			//panggil fungsi aggrobot
+			Aggro("BOT");
+			break;
+		}
+	}
+
+	//fungsi aggro
+	void Aggro(string _platform){
+		//cek posisi x player
+		if (transform.position.x < player.transform.position.x) {
+			//gerak ke kanan
+			//Debug.Log ("KANAN");
+			Move ("RIGHT");
+		}
+		else if (transform.position.x > player.transform.position.x) {
+			//gerak ke kiri
+			//Debug.Log ("KIRI");
+			Move ("LEFT");
+		}
+		//cek posisi y
+		switch(_platform){
+		case "TOP":
+			AggroTop ();
+			break;
+		case "BOT":
+			AggroBot ();
+			break;
+		}
+	}
+
+	void AggroTop(){
+		if (playerScript.state == playerHandler.playerState.BOT) {
+			//Debug.Log ("BAWAH");
+			Jump ("DOWN");
+		}
+	}
+
+	void AggroBot(){
+		if (playerScript.state == playerHandler.playerState.TOP) {
+			//Debug.Log ("ATAS");
+			Jump ("UP");
+		}
+	}
+
+	//fungsi gerak
+	void Move(string _dir){
+		//gerak kanan
+		if (_dir == "RIGHT") {
+			dir = new Vector2 (movSpd, 0f);
+		} 
+		//gerak kiri
+		else if (_dir == "LEFT") {
+			dir = new Vector2 (-movSpd, 0f);
+		}
+		rb2d.AddForce (dir);
+	}
+
+	//fungsi loncat
+	void Jump(string _dir){
+		//loncat keatas
+		if (_dir == "UP") {
+			dir = new Vector2 (0f, jmpSpd);
+		} 
+		//loncat kebawah
+		else if (_dir == "DOWN") {
+			dir = new Vector2 (0f, 0f);
+		}
+		if (canJump) {
+			Physics2D.IgnoreCollision (bc2d, top.GetComponent<BoxCollider2D> (), true);
+			//bc2d.enabled = false;
+			//Debug.Log (dir);
+			rb2d.AddForce (dir);
+		} else if(!canJump && transform.position.y > top.transform.position.y && _dir == "UP"){
+			Physics2D.IgnoreCollision (bc2d, top.GetComponent<BoxCollider2D> (), false);
+		}
+	}
+}
